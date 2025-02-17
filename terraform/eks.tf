2359c383cfa370e5ebf2a900974ef085de615a5f
khoi nguyen <vkhoi444@gmail.com>
2025-02-17 12:46:48 +0700

    first commit

---
+++ terraform/eks.tf
@@ -0,0 +1,40 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "${var.app_name}-${var.env}-eks"
+  cluster_version = var.kubernetes_version
+  
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+
+  vpc_id = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  tags = {
+    cluster = "${var.app_name}-${var.env}-eks"
+  }
+
+ 
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2023_x86_64_STANDARD"
+    instance_types         = ["t2.medium"]
+  }
+
+  eks_managed_node_groups = {
+    node_group = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name                 = "${var.app_name}-${var.env}-vpc"
+  cidr                 = var.vpc_cidr_block
+
+  azs                  = var.azs
+  private_subnets      = var.private_subnets
+  public_subnets       = var.public_subnets
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.app_name}-${var.env}-vpc" = "shared"
+  }
+
+ public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
