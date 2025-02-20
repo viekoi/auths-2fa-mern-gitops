e702e9b4420a8f8ce522de11e731161693132a8a
viekoi <vkhoi444@gmail.com>
2025-02-20 20:57:34 +0700

    add nginx-ingress and cert-manager

---
+++ terraform/nginx-ingress.tf
@@ -0,0 +1,31 @@
+resource "helm_release" "external_nginx" {
+  name = "external"
+
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.10.1"
+
+  set {
+    name  = "controller.ingressClassResource.name"
+    value = "external-nginx"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "external"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
+    value = "ip"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
+
+  depends_on = [helm_release.aws_lbc]
+}
